13a05c6219abca706958f425e46d70b501dd293b
Sumire2020 <sumire2020@163.com>
2020-08-22 01:12:26 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# ControllerViberationTool
+# ControllerRotatorTestTool
+# 手柄震动控制工具
+This is a Xbox format game controller rotator test tool.
+
+Anyone who has ever heard about or used electric toys(vibrating egg/AV rod), must have in a way worried about their concealment.
+
+The direct effect of this project is to turn game controller into electric toy and raise the concealment of pleasure to the highest level.
+
+At present, the function is very simple, and more functions will be updated continuously.
+
+exe file is in the Standalone folder.
+
+Don't ask me how I came up with it ε=ε=ε=┏(゜ロ゜;)┛
+
+有了解或使用过电动玩具(跳蛋/AV棒)的朋友，多少会对其隐蔽性有所顾虑。
+
+这个工程的直接作用是将手柄变为电动玩具，将快乐的隐蔽性提到最高。
+
+目前功能很简陋，更多功能将持续更新。
+
+exe文件在Standalone文件夹里。
+
+不要问我怎么想到的 ε=ε=ε=┏(゜ロ゜;)┛
+
+Ver-1.0 Unity 2019.2.6.f1
---
+++ Ver-1.0/Assets/Script/TestInputVibration.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+public class TestInputVibration : MonoBehaviour
+{
+    private Single leftVib = 0;
+    private Single rightVib = 0;
+    private Single dura = 0.01f;
+
+    private float lIn;
+    private string currentButton;//当前按下的按键
+    private string[] words;
+    //震动的方向
+    XInputDotNetPure.PlayerIndex _type = XInputDotNetPure.PlayerIndex.One;
+
+    private void Start()
+    {
+        Screen.fullScreen = false;
+        XInputDotNetPure.GamePad.SetVibration(_type, 0, 0);
+    }
+
+    private void Update()
+    {
+        var values = Enum.GetValues(typeof(KeyCode));//存储所有的按键
+        for (int x = 0; x < values.Length; x++)
+        {
+            if (Input.GetKeyDown((KeyCode)values.GetValue(x)))
+            {
+                currentButton = values.GetValue(x).ToString();//遍历并获取当前按下的按键
+            }
+        }
+        string tmp;
+        if(currentButton != null)
+        {
+            Debug.Log(currentButton);
+            words = currentButton.Split('n');
+            Debug.Log(words[words.Length - 1]);
+            tmp = words[words.Length - 1];
+        }
+        else
+        {
+            tmp = "0";
+        }
+        if (tmp == "3")
+        {
+            XInputDotNetPure.GamePad.SetVibration(_type, 1, 1);
+        }
+        else if (tmp == "2")
+        {
+            XInputDotNetPure.GamePad.SetVibration(_type, 0.7f, 0.7f);
+        }
+        else if (tmp == "1")
+        {
+            XInputDotNetPure.GamePad.SetVibration(_type, 0.3f, 0.3f);
+        }
+        else if (tmp == "0")
+        {
+            XInputDotNetPure.GamePad.SetVibration(_type, 0, 0);
+        }
+    }
+}
